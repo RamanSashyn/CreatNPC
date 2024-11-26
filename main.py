d42d2688640b040bbe53d3b9827fea8d921dcb73
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-11-26 16:34:58 +0100

    Генерация рандомных характеристик и скилов

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import file_operations
 from faker import Faker
+import random
 
-fake_name = Faker("ru_RU")
-print(fake_name.name())
-name_npc = {
-    "first_name": fake_name.first_name(),
-    "last_name": fake_name.last_name(),
+fake_info = Faker("ru_RU")
+
+skills = ["Стремительный прыжок",
+          "Электрический выстрел",
+          "Ледяной удар",
+          "Стремительный удар",
+          "Кислотный взгляд",
+          "Тайный побег",
+          "Ледяной выстрел",
+          "Огненный заряд"]
+
+npc_info = {
+    "first_name": fake_info.first_name(),
+    "last_name": fake_info.last_name(),
+    "job": fake_info.job(),
+    "town": fake_info.city(),
+    "strength": random.randint(3, 18),
+    "agility": random.randint(3, 18),
+    "endurance": random.randint(3, 18),
+    "intelligence": random.randint(3, 18),
+    "luck": random.randint(3, 18),
+    "skill_1": random.choice(skills),
+    "skill_2": random.choice(skills),
+    "skill_3": random.choice(skills),
 }
 
+file_operations.render_template("template/charsheet.svg", "results/result.svg", npc_info)
